31fc7c2b59cd7d39b610a01cb1aadbff4deef8b9
Domantas Jokūbaitis <domas.jokubaitis@gmail.com>
2024-07-07 18:09:47 +0300

    mail and phone

---
+++ extract.py
@@ -0,0 +1,13 @@
+import pyperclip
+import re
+
+text = pyperclip.paste()
+
+regex = re.compile(r'\S{0,30}?@\S{0,30}|\(?\d{3}\)?[-.\s]?\d{3}[-.\s]?\d{4}|\+\d{3}\s\d{3}\s\d{2}\s\d{3}')
+answer = regex.findall(text)
+
+if not answer:
+    print("Nothing found.")
+
+for i in answer:
+    print(f"{i}", end=" | ")
