7a83c8041700cdf25740d5f6be89a18d2a9393f6
Domantas Jokūbaitis <151384292+DomantasJokubaitis@users.noreply.github.com>
2024-07-12 18:51:26 +0300

    Add files via upload

--- chatgptexer/r_w_json/read_write.py
+++ chatgptexer/r_w_json/read_write.py
@@ -27,16 +27,32 @@ def main():
         contents = []
         
     new_dict = {}
-    name = input("Enter a name: ")
-    age = input("Enter age: ")
-    email = input("Enter an email address: ")
-    new_dict["name"], new_dict["age"], new_dict["email"] = name, age, email
-    contents.append(new_dict)
-
+    commands = ("add", "search", "modify", "delete")
+    wish = input("Add | Search | Modify | Delete :").lower()
+    while wish not in commands:
+        wish = input("Unknown command, try again: ")
+    if "add" in wish:
+        name = str(input("Enter a name: "))
+        age = int(input("Enter age: "))
+        email = input("Enter an email address: ")
+        while "@" not in email:
+            email = input("Invalid email, try again: ")
+        new_dict["name"], new_dict["age"], new_dict["email"] = name, age, email
+        contents.append(new_dict)
+    """elif "modify" in wish:
+        change_person = input("Enter the person's name to modify: ")
+        for dictionary in contents:
+            if dictionary[change_person]:
+                #user should be able to enter what info to modify, like only age or age and mail
+    """
     new_text = json.dumps(contents)
     path.write_text(new_text)
 
 if __name__ == "__main__":
     main()
+
+#implement search function by name
+#implement delete function by name
+#implement detail update function 
 
 
