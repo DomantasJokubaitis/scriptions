e965f9684db5f6d1e6d24a81bc95bf897511d441
Domantas Jokūbaitis <domas.jokubaitis@gmail.com>
2024-07-17 23:56:41 +0300

    main

--- password/strong.py
+++ password/strong.py
@@ -1,7 +1,5 @@
 import re
-import sys
 
-password = input("Enter your desired password: ")
 regex1 = re.compile(r'\S{8,100}')
 regex2 = re.compile(r'[a-z]')
 regex3 = re.compile(r'[A-Z]')
@@ -9,15 +7,32 @@ regex4 = re.compile(r'[0-9]')
 
 regexss = [regex1, regex2, regex3, regex4]
 
-for regex in regexss:
-    answer = regex.search(password)
 
-    if answer == None:
+def check(password):
+    for regex in regexss:
+        answer = regex.search(password)
+
+        if answer == None:
+            return True
+        
+        else:
+            continue
+
+    return False
+
+
+def main():
+    password_weak = True
+    password = input("Enter your desired password: ")
+    password_weak = check(password)
+    
+    if password_weak:
         print("Password is weak")
-        sys.exit()
     else:
-        continue
+        print("Password is strong")
+    
 
-print("Password strength: Strong")
+if __name__ == "__main__":
+    main()
 
 
--- pattern/patternmatch.py
+++ pattern/patternmatch.py
@@ -5,6 +5,11 @@ path = Path(r"pattern/phones.txt")
 with open(path, "r") as f:
     text = f.read()
 
-phone_num_regex = re.compile(r'\(?\d{3}\)?[-.\s]?\d{3}[-.\s]?\d{4}')
-answer = phone_num_regex.findall(text)
-print(answer)
+def main():
+
+    phone_num_regex = re.compile(r'\(?\d{3}\)?[-.\s]?\d{3}[-.\s]?\d{4}')
+    answer = phone_num_regex.findall(text)
+    print(answer)
+
+if __name__ == "__main__":
+    main()
