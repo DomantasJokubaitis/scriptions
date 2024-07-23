941a3cc059bbef3dfc54060093dc58fe1e812608
Domantas Jokubaitis <domas.jokubaitis@gmail.com>
2024-07-23 14:09:33 +0300

    start

--- am_eu_date_regex/am2eu.py
+++ am_eu_date_regex/am2eu.py
@@ -1,1 +1,16 @@
 import re, os
+from pathlib import Path
+
+
+path = Path("am_eu_date_regex")
+file_paths = os.listdir(path)
+matches = []
+
+
+regex = re.compile(r"\d{2}-\d{2}-\d{4}")
+for item in file_paths:
+    answer = regex.search(item)
+    if answer:
+        print(answer.group())
+
+# testing
