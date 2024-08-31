b16b544ef5deb7b74e1278299d76bd0718a3f0b4
DomantasJokubaitis <domas.jokubaitis@gmail.com>
2024-08-31 21:03:28 +0300

    windows(ucks)

--- am_eu_date_regex/am2eu.py
+++ am_eu_date_regex/am2eu.py
@@ -11,14 +11,17 @@ def main():
 
     for item in file_paths:
         answer = regex.search(item)
+
         if answer:
             am_date = answer.group()
 
             full_old_path = os.path.join(path, item)
 
             am_date_list = am_date.split("-")
+
             if am_date_list[0] == am_date_list[1] or int(am_date_list[0]) > 12:
                 continue
+
             mm, dd = am_date_list[0], am_date_list[1]
             eu_date = f"{dd}-{mm}-{am_date_list[2]}"
             
